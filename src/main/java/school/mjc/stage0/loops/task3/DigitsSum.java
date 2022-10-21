39438721cf99bdd6343803f1b62ec573d8362e20
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-21 08:54:59 +0500

    3
    ###

--- src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/DigitsSum.java
@@ -3,6 +3,7 @@ package school.mjc.stage0.loops.task3;
 public class DigitsSum {
     public void printDigitsSum(int t){
         int sum=0;
+        t=Math.abs(t);
         String s=String.valueOf(t);
         for (int i=0; i<s.length(); i++){
             sum += t%10;
