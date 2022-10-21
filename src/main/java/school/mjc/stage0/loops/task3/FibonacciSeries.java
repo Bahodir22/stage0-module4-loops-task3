528cb33ced368406a20d0ac000f197348ebbd2eb
Bahodir Abdusaxatov <88647942+Bahodir22@users.noreply.github.com>
2022-10-21 06:23:28 +0500

    1
    #

--- src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
+++ src/main/java/school/mjc/stage0/loops/task3/FibonacciSeries.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.loops.task3;
 
 public class FibonacciSeries {
     public void printFibonacci(int lastFibonacci) {
+        int first=1,second=1;
+        System.out.println(first);
+        System.out.println(second);
+        for (int i=0; i<lastFibonacci; i++){
+            first=second;
+            second=first+second;
+            System.out.println(first);
+            System.out.println(second);
+        }
     }
 }
--- src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
+++ src/main/java/school/mjc/stage0/loops/task3/NinesSum.java
@@ -2,6 +2,11 @@ package school.mjc.stage0.loops.task3;
 
 public class NinesSum {
     public void calculateSum(int lengthOfLastNumber) {
-
+        int sum=0;
+        for (int i=0, k=1; i<lengthOfLastNumber; i++){
+            sum+=(Math.pow(10,k)-1);
+            k++;
+        }
+        System.out.println(sum);
     }
 }
